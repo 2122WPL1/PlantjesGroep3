165e9e62ac49f01e48a4f690a1c4783ce84ffa11
Isabelle Vandooren <u0081592@vives.be>
2022-02-20 18:26:01 +0100

    Add project files.

---
+++ Plantjes/Dao/SQLConnection.cs
@@ -0,0 +1,10 @@
+﻿namespace Plantjes.Dao
+{
+    internal class SQLConnection
+    {
+        //plaats hier de connectiestring naar je databank
+        internal static string connectionstring = 
+       
+
+    }
+}
