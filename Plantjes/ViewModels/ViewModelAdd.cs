b9184c665e111a20fad4b0377bfe6e69570ddd54
WarreDescamps <r0899637@student.vives.be>
2022-03-08 09:32:47 +0100

    Add daologic to viewmodeladd

--- Plantjes/ViewModels/ViewModelAdd.cs
+++ Plantjes/ViewModels/ViewModelAdd.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plantjes.Models.Db;
+using Plantjes.Dao;
 
 namespace Plantjes.ViewModels
 {
     internal class ViewModelAdd : ViewModelBase
     {
+        private readonly DAOLogic dao;
 
+        public ViewModelAdd()
+        {
+            dao = DAOLogic.Instance();
+        }
     }
 }
