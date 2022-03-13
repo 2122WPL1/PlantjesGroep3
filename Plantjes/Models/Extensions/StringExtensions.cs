28e1d27fc5bc17163a32efaef3b92c3d0e8c8b39
Warre Descamps <r0899637@student.vives.be>
2022-03-13 20:31:54 +0100

    Add FirstToUpper StringExtension

---
+++ Plantjes/Models/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plantjes.Models.Extensions
+{
+    internal static class StringExtensions
+    {
+        public static string FirstToUpper(this string input) => input.Length > 1 ? input[..0].ToUpper() + input[1..] : input.ToUpper();
+    }
+}
