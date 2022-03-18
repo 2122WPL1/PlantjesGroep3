50425104bd42548f1b42c30f9f863e2f9859e011
Renzo Dekimpe <renzo.dekimpe95@hotmail.com>
2022-03-18 10:42:46 +0100

    Adjust DAOPlant, change nullables

--- Plantjes/Dao/DAOplant.cs
+++ Plantjes/Dao/DAOplant.cs
@@ -9,16 +9,22 @@ namespace Plantjes.Dao
 {
     public class DaoPlant : DaoBase
     {
-        public static Plant AddPlant(string type, string familie, string geslacht, string soort, string variant)
+        public static Plant AddPlant(string type, string familie, string geslacht, string soort = null, string variant = null)
         {
             Plant plant = new Plant()
             {
                 Type = type,
                 Familie = familie,
-                Geslacht = geslacht,
-                Soort = soort,
-                Variant = variant
+                Geslacht = geslacht
             };
+            if (soort != null)
+            {
+                plant.Soort = soort;
+            }
+            if (variant != null)
+            {
+                plant.Variant = variant;
+            }
             context.Plants.Add(plant);
             context.SaveChanges();
             return plant;
