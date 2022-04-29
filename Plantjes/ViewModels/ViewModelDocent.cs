ee2dc3789d42e18e96a02448082692b53ef72be7
Renzo Dekimpe <renzo.dekimpe95@hotmail.com>
2022-04-29 09:02:05 +0200

    Design Docent-Admin screen

---
+++ Plantjes/ViewModels/ViewModelDocent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Plantjes.Models.Classes;
+
+namespace Plantjes.ViewModels
+{
+    internal class ViewModelDocent:ViewModelBase
+    {
+        private ObservableCollection<GroupBox> Notificaties;
+
+        public ViewModelDocent()
+        {
+            Notificaties = new ObservableCollection<GroupBox>() { new PlantNotification(" ") };//TODO add parameter for object
+        }
+
+        private void AddNotificatie()
+        {
+            Notificaties.Add(new PlantNotification(""));
+        }
+    }
+}
