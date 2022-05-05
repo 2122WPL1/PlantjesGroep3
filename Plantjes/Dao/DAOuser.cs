0e12201dcb2c6847f673dd9d9aedb70d69d8e843
Warre Descamps <r0899637@student.vives.be>
2022-05-05 14:01:49 +0200

    Update role permission height

--- Plantjes/Dao/DAOuser.cs
+++ Plantjes/Dao/DAOuser.cs
@@ -65,11 +65,11 @@ namespace Plantjes.Dao
         {
 
             //written by Warre
-            int role = 2;
+            int role = 3;
             if (emailadres.ToLower().Contains("@vives.be"))
-                role = 0;
-            if (emailadres.ToLower().Contains("@student.vives.be"))
                 role = 1;
+            if (emailadres.ToLower().Contains("@student.vives.be"))
+                role = 2;
 
             var gebruiker = new Gebruiker()
             {
