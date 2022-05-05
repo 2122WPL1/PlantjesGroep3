1a46dfcd1457a1dd2b48549f2f9a1803e95cf4d6
Warre Descamps <r0899637@student.vives.be>
2022-05-05 09:12:09 +0200

    Add plant detail

---
+++ Plantjes/ViewModels/ViewModelPlantDetail.cs
@@ -0,0 +1,19 @@
+﻿using Plantjes.Models.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plantjes.ViewModels
+{
+    public class ViewModelPlantDetail : ViewModelBase
+    {
+        public ViewModelPlantDetail(Plant plant)
+        {
+            Plant = plant;
+        }
+
+        public Plant Plant { get; private set; }
+    }
+}
