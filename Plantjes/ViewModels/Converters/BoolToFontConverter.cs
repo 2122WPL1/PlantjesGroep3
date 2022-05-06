00051914750d1cb5646578d9274348010cae6dca
Warre Descamps <r0899637@student.vives.be>
2022-05-06 13:51:51 +0200

    Add modular eigenschappen to details

---
+++ Plantjes/ViewModels/Converters/BoolToFontConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Plantjes.ViewModels.Converters
+{
+    internal class BoolToFontConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isChecked && isChecked)
+                return new FontFamily("Segoe UI");
+            var font = new FontFamily(new Uri("file:///" + AppDomain.CurrentDomain.BaseDirectory + @"Font", UriKind.Absolute), "./password");
+            return font;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Plantjes/ViewModels/ViewModelPlantDetail.cs
+++ Plantjes/ViewModels/ViewModelPlantDetail.cs
@@ -1,10 +1,13 @@
-﻿using Plantjes.Models.Db;
+﻿using Plantjes.Models.Classes;
+using Plantjes.Models.Db;
 using Plantjes.Models.Extensions;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace Plantjes.ViewModels
@@ -12,9 +15,17 @@ namespace Plantjes.ViewModels
     //Written by Renzo
     public class ViewModelPlantDetail : ViewModelBase
     {
+        private List<StackPanel> _eigenschappen;
+
         public ViewModelPlantDetail(Plant plant)
         {
             Plant = plant;
+            _eigenschappen = new List<StackPanel>();
+            _eigenschappen.Add(new PlantEigenschap<Fenotype, FenotypeMulti>(plant.Fenotypes, plant.FenotypeMultis));
+            _eigenschappen.Add(new PlantEigenschap<Abiotiek, AbiotiekMulti>(plant.Abiotieks, plant.AbiotiekMultis));
+            _eigenschappen.Add(new PlantEigenschap<Commensalisme, CommensalismeMulti>(plant.Commensalismes, plant.CommensalismeMultis));
+            _eigenschappen.Add(new PlantEigenschap<BeheerMaand, object>(plant.BeheerMaands));
+            _eigenschappen.Add(new PlantEigenschap<ExtraEigenschap, object>(plant.ExtraEigenschaps));
         }
 
         public string PlantNaam
@@ -25,5 +36,7 @@ namespace Plantjes.ViewModels
         public Plant Plant { get; private set; }
         
         public BitmapImage PlantImage { get => Plant.GetPlantImage(); }
+
+        public List<StackPanel> Eigenschappen { get => _eigenschappen; }
     }
 }
