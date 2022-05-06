da5320afa858ba44b850b580a5a6b73409f38a4e
Ian Dumalin <r0657073@student.vives.be>
2022-05-06 09:16:50 +0200

    Revert "Fixed role assignment"
    This reverts commit cc4b5b017bbf6f6a4d9c6ec5289e9f86682ade63.

--- Plantjes/Dao/DAOuser.cs
+++ Plantjes/Dao/DAOuser.cs
@@ -41,11 +41,11 @@ namespace Plantjes.Dao
             var passwordHashed = Helper.HashString(password);
 
             //written by Warre
-            int role = 3;
+            int role = 2;
             if (emailadres.ToLower().Contains("@vives.be"))
-                role = 1;
+                role = 0;
             if (emailadres.ToLower().Contains("@student.vives.be"))
-                role = 2;
+                role = 1;
 
             var gebruiker = new Gebruiker()
             {
